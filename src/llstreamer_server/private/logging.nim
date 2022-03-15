1d34cbbc245abb04754b26e99bd05e5603f8553a
= <termer@protonmail.com>
2022-03-15 08:56:18 -0400

    flush after writing to stdout/stderr

--- src/llstreamer_server/private/logging.nim
+++ src/llstreamer_server/private/logging.nim
@@ -4,16 +4,19 @@ proc logInfo*(msg: string) =
     ## Logs an info message
 
     stdout.styledWriteLine(styleBright, fgGreen, "[I]", resetStyle, " "&msg)
+    flushFile(stdout)
 
 proc logError*(msg: string) =
     ## Logs an error message
 
     stderr.styledWriteLine(styleBright, fgRed, "[E]", resetStyle, fgRed, " "&msg, resetStyle)
+    flushFile(stderr)
 
 proc logWarn*(msg: string) =
     ## Logs a warning message
     
     stderr.styledWriteLine(styleBright, fgYellow, "[W]", resetStyle, fgYellow, " "&msg, resetStyle)
+    flushFile(stderr)
 
 proc logError*(msg: string, exception: ref Exception, exceptionMsg: string) =
     ## Logs an error message with an exception
@@ -21,3 +24,4 @@ proc logError*(msg: string, exception: ref Exception, exceptionMsg: string) =
     logError(msg)
     logError("\tException: "&repr(exception))
     logError("\tMessage: "&exceptionMsg)
+    flushFile(stderr)
